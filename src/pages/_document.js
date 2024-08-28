eedb1a3fe24cbc4062fb9c6303ae2620cc649193
Abhijeet kumar <abhijeet122kumar@gmail.com>
2024-08-28 14:31:09 +0530

    mea

---
+++ src/pages/_document.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+        <script defer type="text/javascript" src="https://deepesh2.community.druidplatform.com/v2/druid_webchat_v2_load.js?botId=aff0134f-34d4-4a14-4711-08dc65bdd66e&amp;baseUrl=https%3A%2F%2Fdruidapi.comm.eu.druidplatform.com">
+        </script>
+      </body>
+    </Html>
+  );
+}
