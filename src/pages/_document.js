9c1fa8031b5ed6e9143c88e1320c362e627caff6
Abhijeet kumar <abhijeet122kumar@gmail.com>
2024-09-17 17:40:43 +0530

    first commit

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,7 +7,7 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <script defer type="text/javascript" src="https://deepesh2.community.druidplatform.com/v2/druid_webchat_v2_load.js?botId=53b0e4e2-04f9-42aa-45b6-08dc2e003ded&amp;baseUrl=https%3A%2F%2Fdruidapi.comm.eu.druidplatform.com">
+        <script defer type="text/javascript" src="https://Azadea.community.druidplatform.com/v2/druid_webchat_v2_load.js?botId=d5fa36d2-d070-4053-9806-08dcd2fea4b4&amp;baseUrl=https%3A%2F%2Fdruidapi.comm.eu.druidplatform.com">
         </script>
       </body>
     </Html>
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <>
     <Head>
-      <title>HealthCare</title>
+      <title>Azadea</title>
     </Head>
       <div>
         <h4></h4>
