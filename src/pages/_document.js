0d40c90a9da12ab9162e552f458c73b59a00ae99
Abhijeet kumar <abhijeet122kumar@gmail.com>
2024-09-26 17:45:17 +0530

    First

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,7 +7,7 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <script defer type="text/javascript" src="https://Azadea.community.druidplatform.com/v2/druid_webchat_v2_load.js?botId=d5fa36d2-d070-4053-9806-08dcd2fea4b4&amp;baseUrl=https%3A%2F%2Fdruidapi.comm.eu.druidplatform.com">
+        <script defer type="text/javascript" src="https://Abhijeet.community.druidplatform.com/v2/druid_webchat_v2_load.js?botId=ba4f247e-67e0-4697-791f-08dcdcb6785e&amp;baseUrl=https%3A%2F%2Fdruidapi.comm.eu.druidplatform.com">
         </script>
       </body>
     </Html>
